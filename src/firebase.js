3791fbeb09d20cda7ee90adf4c4e76e20439da9d
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-06-22 11:18:41 +0530

    adding firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBx-b4rvwDFh5cHzPuWzl-x-nktoJp7cww",
+    authDomain: "ecommerce-react-dc772.firebaseapp.com",
+    projectId: "ecommerce-react-dc772",
+    storageBucket: "ecommerce-react-dc772.appspot.com",
+    messagingSenderId: "101094245362",
+    appId: "1:101094245362:web:9166654d4bc97a6677e16b"
+  };
