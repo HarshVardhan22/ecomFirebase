37a1f32bf334555b80bf8d308fb39ad4cdc69589
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-06-27 03:15:35 +0530

    +fixed header userName +signOut

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import "./App.css";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
-import Login from "./Login"
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+
 function App() {
+  const [{}, dispatch] = useStateValue();
+  //the useEffect && contextAPI will be used to keep track of which user is logged in currently
+  //so that we know on each page which user is logged in and will show personalized data like cart items
+  //and the session is maintained even if the page is refreshed
+  useEffect(() => {
+    //will only run once when the app comp is loaded
+    auth.onAuthStateChanged((authUser) => {
+      console.log("the user is authUser", authUser);
+      
+      if (authUser) {
+        //the user just logged is or was already logged in
+        dispatch({
+          type:'SET_USER',
+          user: authUser
+        });
+      } else {
+        //the user logged out
+        dispatch({
+          type:'SET_USER',
+          user: null
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route path="/login">
-            <Login/>
+            <Login />
           </Route>
           <Route path="/checkout">
             <Header />
--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,7 @@ import Subtotal from './Subtotal';
 import {useStateValue} from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 function Checkout() {
-    const [{basket},dispatch] = useStateValue();
+    const [{basket,user},dispatch] = useStateValue();
 
     return (
         <div className="checkout">
