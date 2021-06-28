b21282a822fa3b7c6655d95c3de1701bae8cbcc2
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-06-29 02:35:48 +0530

    react-reveal added

--- src/Header.js
+++ src/Header.js
@@ -17,10 +17,10 @@ function Header() {
 
   return (
 
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-dark header" style={{zIndex:"100",top:"0"}} >
       <Link to="/">
         <div>
-          <img src={brand}  className="brandLogo" alt="BrandLogo" />
+          <img src={brand} className="brandLogo" alt="BrandLogo" />
           <span className="text-info brandText">ReactKart</span>
         </div>
       </Link>
@@ -39,16 +39,19 @@ function Header() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
 
-          <li className="nav-item active">
-            <span className="header__optionLineTwo">
-              Hello {user ? user.email : "Guest"}
-            </span>
+          <li className="nav-item">
+            <div className="nav_container">
+              <span className="nav-link header__optionLineTwo">
+                Hello {user ? user.email : "Guest"}
+              </span>
+            </div>
           </li>
+
 
           <li className="nav-item active">
             <Link to={!user && "/login"}>
               <div onClick={handleAuthentication} >
-                <span className="header__optionLineTwo">
+                <span className="nav-link header__optionLineTwo">
                   {user ? "Sign Out" : "Sign In"}
                 </span>
               </div>
@@ -57,27 +60,27 @@ function Header() {
 
           <li className="nav-item">
             <Link to="/addProduct">
-              <span className="header__optionLineTwo">Add Products</span>
+              <span className="nav-link header__optionLineTwo">Add Products</span>
             </Link>
           </li>
 
           <li className="nav-item active">
             <Link to="/checkout">
-              <div className="header__optionBasket">
+              <div className="nav-link header__optionLineTwo">
                 <ShoppingCartIcon className="shoppingCart__icon" />
-                <span className="header__optionLineTwo header__basketCount">
+                <span className="header__basketCount">
                   {basket?.length}
                 </span>
               </div>
             </Link>
           </li>
-          
+
         </ul>
-          {/* ***********Can add Add Products option for admins only unless disabled********** 
+        {/* ***********Can add Add Products option for admins only unless disabled********** 
        <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
+        <a className="nav-link header__optionLineTwo disabled" href="#">Disabled</a>
       </li> */}
-       
+
         {/* <form className="form-inline my-2 my-lg-0">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           <SearchIcon className="search__icon" />
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import cover from "./assets/cover.png";
+import cover from "./assets/cover.jpg";
 import { db } from "./firebase";
 import "./Home.css";
 import Product from "./Product";
 import { useStateValue } from "./StateProvider";
+import Fade from 'react-reveal/Fade';
 
 function Home() {
 
@@ -15,14 +16,14 @@ function Home() {
     data.docs.map((item) => {
       console.log(item.data().productImage)
       setProducts((prevState) => {
-        return [...prevState,item.data()]
+        return [...prevState, item.data()]
       })
     });
   };
 
   useEffect(() => {
     fetchProducts();
-  },[]);
+  }, []);
 
 
 
@@ -31,21 +32,26 @@ function Home() {
   return (
     <div className="home container-fluid">
       <div className="home__container">
-        <img src={cover} alt="" className="home__image" />
+        <Fade top>
+          <p className='text-white home__text'>Lorem ipsum dolor sit amet</p>
+          <p className='text-white home__text2'>Excepteur sint occaecat</p>
+
+        </Fade>
+
       </div>
       <div className="row">
-       
+
         {products.map((id) => {
-        return (
-          <div className="col-lg-4 col-l-4 col-md-6 col-s-6 col-xs-12">
-          <Product title={id.productName} price={id.productPrice} 
-        rating= {Math.floor(Math.random() * 2)+3} image={id.productImage}/>
-          </div>
-         );
-      })}
-        
+          return (
+            <div className="col-lg-4 col-l-4 col-md-6 col-s-6 col-xs-12">
+              <Product title={id.productName} price={id.productPrice}
+                rating={Math.floor(Math.random() * 2) + 3} image={id.productImage} />
+            </div>
+          );
+        })}
+
       </div>
-      
+
     </div>
   );
 }
